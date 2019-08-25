e0a3cf39946525bd3dc527db6e52e08e4a7d1884
pikaChin <derrfgh4563@gmail.com>
2019-08-25 23:56:41 +0800

    Edit and Submit and modify complete

--- app/src/main/java/com/example/edittextpractice/MainActivity.java
+++ app/src/main/java/com/example/edittextpractice/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.edittextpractice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sayHello(View v) {
+        TextView text = (TextView) findViewById(R.id.txv);
+        EditText input = (EditText) findViewById(R.id.edit);
+
+        String output = input.getText().toString();
+
+        if (output.length() == 0) {
+            text.setText(R.string.hello);
+        }
+        else {
+            output = output + ", Hello";
+            text.setText(output);
+        }
+
     }
 }
